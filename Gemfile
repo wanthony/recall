9f2a47c69e7151ec2eb5daaea30cc6bf6a634c3e
Anthony White <anthony.white.j@gmail.com>
2011-08-22 23:14:03 -0400

    Added Gemfile.lock (bundle install'd), and updated deps

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source :rubyforge
 
 gem 'sinatra'
 gem 'thin'
-gem 'erb'
 gem 'less'
 gem 'coffee-script'
--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'erb'
+require 'coffee'
+require 'less'
 
 get '/' do
-
+  erb :index
 end
